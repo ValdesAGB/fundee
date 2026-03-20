145243426cd67f2e4ad880f157fcf28a4192a429
ValdesAGB <valdesagbokoni01@gmail.com>
2026-03-20 08:40:03 +0100

    Without Forgot Password Logique

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,19 @@
 // middleware.ts (racine)
 import { NextRequest, NextResponse } from "next/server";
 
-const PUBLIC_ROUTES = ["/admin/login", "/admin/register", "/admin/forgot-password"];
+const PUBLIC_ROUTES = [
+  "/admin/login",
+  "/admin/register",
+  "/admin/forgot-password",
+];
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   const isPublic = PUBLIC_ROUTES.some((route) => pathname.startsWith(route));
-  const token = req.cookies.get("better-auth.session_token")?.value;
+  const token =
+    req.cookies.get("better-auth.session_token")?.value ||
+    req.cookies.get("__Secure-better-auth.session_token")?.value;
 
   if (!isPublic && !token) {
     return NextResponse.redirect(new URL("/admin/login", req.url));
